602efe3cc6e3350f0248c16bc928d873c9d79269
KArunKumar <arunkkarukolla@gmail.com>
2026-06-01 23:14:35 +0530

    Add insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO Users VALUES
+(1,'Arun','arun@gmail.com','India'),
+(2,'John','john@gmail.com','USA'),
+(3,'Sara','sara@gmail.com','UK');
+
+INSERT INTO Genres VALUES
+(1,'Action'),
+(2,'Comedy'),
+(3,'Drama');
+
+INSERT INTO Movies VALUES
+(101,'Extraction',2020,8.0),
+(102,'Red Notice',2021,7.5),
+(103,'The Irishman',2019,8.2);
+
+INSERT INTO Movie_Genres VALUES
+(101,1),
+(102,2),
+(103,3);
+
+INSERT INTO Subscriptions VALUES
+(1,1,'Premium','2025-01-01'),
+(2,2,'Basic','2025-02-01'),
+(3,3,'Standard','2025-03-01');
